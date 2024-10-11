d4b7766691aee2357b0c004354c5218fc68c1a07
ansarworkingspace <ansarworkingspace@gmail.com>
2024-10-11 16:11:37 +0530

    form building

--- components/form/patientForm.tsx
+++ components/form/patientForm.tsx
@@ -1,10 +1,69 @@
+"use client"
 
-import React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-const PatientForm = () => {
-    return (
-        <div>PatientForm</div>
-    )
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+const  PatientForm = () =>{
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+     
+     <section className="mb-12 space-y-4">
+      <h1 className="header">Hi there 👋</h1>
+      <p className="text-dark-700">Schedule your first appointment.</p>
+     </section>
+     
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
+  )
 }
-  
-export default PatientForm;
+
+export default PatientForm
