6fc09254048902150568b0535d49331e10e4a7c8
ansarworkingspace <ansarworkingspace@gmail.com>
2024-10-11 20:44:04 +0530

    dev

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  } from "@/components/ui/form"
+  import { Input } from "@/components/ui/input"
+
+const CustomFormField = () => {
+    return (
+        <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    )
+}
+
+export default CustomFormField
