c08517fc9ec944cfd9419c88b89d6c1cc3bfc235
ansarworkingspace <ansarworkingspace@gmail.com>
2025-02-04 11:05:29 +0530

    sms feature implimented

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -5,8 +5,9 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
-import { parseStringify } from "../utils";
+import { formatDateTime, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
 import { Appointment } from "@/types/appwrite.types";
 
@@ -83,26 +84,51 @@ export const getRecentAppointmentList = async () => {
   }
 };
 
+export const updateAppointment = async ({
+  appointmentId,
+  userId,
+  appointment,
+  type,
+}: UpdateAppointmentParams) => {
+  try {
+    const updatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
 
-export const updateAppointment = async ({appointmentId,userId,appointment,type}:UpdateAppointmentParams)=>{
-try {
-  const updatedAppointment = await databases.updateDocument(
-    DATABASE_ID!,
-    APPOINTMENT_COLLECTION_ID!,
-    appointmentId,
-    appointment
-  )
+    if (!updatedAppointment) {
+      throw new Error("Appointment not found");
+    }
 
-  if(!updatedAppointment){
-    throw new Error ('Appointment not found')
+    const smsMessage = `Hi, it's QuickConsult. ${
+      type === "schedule"
+        ? `Your appointment has been scheduled for ${formatDateTime(
+            appointment.schedule!
+          ).dateTime} with Dr. ${appointment.primaryPhysician}`
+        : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`
+    }`;
+
+    await sendSMSNotification(userId, smsMessage);
+
+    revalidatePath("/admin");
+    return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  //TODO SMS notification
-  revalidatePath('/admin')
-  return parseStringify(updatedAppointment)
-
-} catch (error) {
-  console.log(error);
-  
-}
-}
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.log(error);
+  }
+};
